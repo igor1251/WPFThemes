1b3d4b6fbeb45445318b57580ed0f6daa06bfd2d
igor1251 <84015785+igor1251@users.noreply.github.com>
2022-12-15 17:03:24 +0300

    Created

---
+++ Theme.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace MacLikeUI
+{
+    public partial class WindowStyle : ResourceDictionary
+    {
+        public WindowStyle()
+        {
+            InitializeComponent();
+        }
+
+        private void PART_MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            window.WindowState = WindowState.Minimized;
+        }
+
+        private void PART_MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            if (window.WindowState == WindowState.Normal)
+            {
+                window.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                window.WindowState = WindowState.Normal;
+            }
+        }
+
+        private void PART_CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            window.Close();
+        }
+    }
+}
